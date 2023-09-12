578547b538499655749e8ede6d2278f2b71d9098
enrique <enriqueadhemar@gmail.com>
2023-09-12 09:32:11 -0400

    generacion mapa

--- Mapa.gd
+++ Mapa.gd
@@ -1,9 +1,24 @@
 extends GridMap
 
+# Escena que deseas instanciar en cada celda
+@onready var cell_scene = preload("res://scenes/vaca.tscn")
+@export var size: int
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	# Obtén el tamaño del GridMap en términos de celdas
+	var grid = []
+	
+	for i in range(-size, size+1):
+		for j in range(-size, size+1):
+			grid.append(Vector3(i,0,j))
+	# Recorre todas las celdas del GridMap
+	for cell_pos in grid:
+		print(cell_pos)
+		# Instancia la escena en la posición de la celda
+		var instance = cell_scene.instantiate()
+		instance.global_transform.origin = cell_pos
+		add_child(instance)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
