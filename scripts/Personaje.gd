66bd0920ba8b03ab9e090afe0da4d73dbfc4fbc7
Brenda <abrendakhenya@gmail.com>
2023-09-12 12:13:33 -0700

    movimiento

--- scripts/Personaje.gd
+++ scripts/Personaje.gd
@@ -6,11 +6,18 @@ const JUMP_VELOCITY = 4.5
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
  
+@onready var camera: Camera3D = $Camera3D
+ 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
  
  
 func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * .005)
+		camera.rotate_x(-event.relative.y * .005)
+		camera.rotation.x = clamp(camera.rotation.x, -PI/4, PI/4)
+ 
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().quit()
  
@@ -28,9 +35,6 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "forward", "back")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-#	print(velocity.x)
-#	print(velocity.y)
-#	print(velocity.z)
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
