a65ab819e81048e66a25310b93d888893bc243e4
Khenya <abrendakhenya@gmail.com>
2023-09-13 21:57:38 -0400

    destruccion bloque

--- scripts/Bloque.gd
+++ scripts/Bloque.gd
@@ -1,6 +1,11 @@
 extends RigidBody3D
 
-@export var recistencia: int
+@export var resistencia: int
 @export var tipo: String
 @export var vida: int 
 
+func destroy():
+	if vida > 0:
+		vida -= resistencia
+	if vida <= 0:
+		queue_free()  
